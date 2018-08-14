8e30bf24ea8c8fce52c9e75ae6846ba802aef1b3
Brandon Green <brandon@brandonleeproductions.com>
2018-08-14 23:51:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,4 +65,11 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
 end
