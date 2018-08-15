9249e7dc4312e983ce119710f484688fc9a373b2
Brandon Green <brandon@brandonleeproductions.com>
2018-08-15 00:15:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,11 +91,14 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT 1
+      LIMIT
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
+
+
+
 end
